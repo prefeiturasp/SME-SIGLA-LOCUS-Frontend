39a633b216022551e0a2d5a17e83d479edc079d3
Vitor Campos <vitor.campos@spassu.com.br>
2026-09-11 10:25:38 -0300

    feat(156204): Adição d e painel e tela de histórico de alterações da UE

--- src/paginas/DetalheUnidadeEducacional/index.tsx
+++ src/paginas/DetalheUnidadeEducacional/index.tsx
@@ -11,6 +11,7 @@ import { BannerVersaoHistorica } from "./componentes/BannerVersaoHistorica";
 import { CardComponentesDetalhe } from "./componentes/CardComponentesDetalhe";
 import { CardInformacoesUnidade } from "./componentes/CardInformacoesUnidade";
 import { LinhaHistoricoAlteracoes } from "./componentes/LinhaHistoricoAlteracoes";
+import { PainelHistorico } from "./componentes/PainelHistorico";
 import { useDetalheUnidade } from "./hooks/useDetalheUnidade";
 
 export function DetalheUnidadeEducacional() {
@@ -114,6 +115,13 @@ export function DetalheUnidadeEducacional() {
 
         <LinhaHistoricoAlteracoes aoAbrir={estado.abrirPainelHistorico} />
       </ConteudoPagina>
+
+      <PainelHistorico
+        aberto={estado.painelHistoricoAberto}
+        registros={estado.historico}
+        aoFechar={estado.fecharPainelHistorico}
+        aoVisualizar={estado.visualizarVersao}
+      />
 
       <ModalConfirmacao
         aberto={estado.modalSaidaAberto}
